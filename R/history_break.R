eddbdab99e50ad4b4bcda57be592532365602bdf
U-ways <ue95.career@gmail.com>
2019-10-10 14:40:30 +0100

    add GPL

--- R/history_break.R
+++ R/history_break.R
@@ -1,6 +1,20 @@
-##################################
-## Bai & Perron last breakpoint ##
-##################################
+# history_break - Bai & Perron last breakpoint
+# Copyright (C) 2019  Marius Appel, Jan Verbesselt
+#
+# This file is part of Foobar.
+#
+# Foobar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 history_break <- function(formula, data, h = NULL, hpc = "none") {
   n <- nrow(data)
--- R/seasonal.R
+++ R/seasonal.R
@@ -1,3 +1,21 @@
+# seasonal
+# Copyright (C) 2019  Dainius Masiliūnas, Jan Verbesselt
+#
+# This file is part of Foobar.
+#
+# Foobar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 seasonal <- function (x, out, sim = NULL, labels = colnames(X),
 set.pars = list(tck = -0.01, mar = c(0, 6, 0, 6), oma = c(6, 0, 4, 0)),
 main = NULL, range.bars = FALSE, ..., col.range = "light gray", fit = NULL)
