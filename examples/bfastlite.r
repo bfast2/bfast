14e4512f8ef01ca36152eeeb28d46246acdde0c1
Dainius <pastas4@gmail.com>
2024-10-21 22:45:48 +0200

    Add raster example to ?bfastlite

--- examples/bfastlite.r
+++ examples/bfastlite.r
@@ -26,3 +26,33 @@ plot(bp)
 
 # Details of the structural change test with the type RE
 bfastlite(datats, level=0.05, type="RE")$sctest
+
+\donttest{
+## Run bfastlite() on a raster
+f <- system.file("extdata/modisraster.tif", package="bfast")
+modisbrick <- terra::rast(f)
+
+# Run on pixel 10
+data <- unlist(modisbrick[10])
+ndvi <- bfastts(data, dates, type = c("16-day"))
+bfl <- bfastlite(ndvi, breaks = "BIC")
+# Get max magnitude by RMSD
+max(magnitude(bfl[["breakpoints"]])$Mag[,"RMSD"])
+
+# Wrapper function
+bflSpatial <- function(pixels)
+{
+    ts <- bfastts(pixels, dates, type = c("16-day"))
+    bfl <- bfastlite(ts, breaks="BIC")
+    bp <- bfl[["breakpoints"]]
+    # Return number of breakpoints and max breakpoint magnitude
+    if (length(bp[["breakpoints"]]) == 1 && is.na(bp[["breakpoints"]]))
+        return(c(0, 0))
+    
+    return(c(length(bp[["breakpoints"]]), max(magnitude(bp)$Mag[,"RMSD"])))
+}
+
+# Run function on each raster pixel
+rastbfl <- terra::app(modisbrick, bflSpatial)
+terra::plot(rastbfl)
+}
